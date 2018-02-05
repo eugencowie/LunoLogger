08c1d0c2a28b8ef99e3e29c6c58c7c68f46a7717
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-02-05 10:11:26 +0000

    [go] Add html template for tablular output

--- lunologger-go/pages/show.go
+++ lunologger-go/pages/show.go
@@ -1,20 +1,49 @@
 package pages
 
 import "net/http"
+import "io/ioutil"
+import "html/template"
 import "model"
-import "fmt"
+
+type ShowTemplate struct {
+	Columns int
+	XbtZarTickers []model.Ticker
+	EthXbtTickers []model.Ticker
+}
 
 func ShowXbtZar(response http.ResponseWriter, request *http.Request) {
+	funcs := template.FuncMap{"divide": func(a, b int) int { return a / b }}
+	contents, err := ioutil.ReadFile("pages/show.html")
+	if err != nil { panic(err) }
+	templ, err := template.New("show").Funcs(funcs).Parse(string(contents))
+	if err != nil { panic(err) }
 	tickers := model.RetrieveTickers(request, "XBTZAR", "-Time")
-	for _, ticker := range tickers { fmt.Fprint(response, ticker.Json) }
+	data := ShowTemplate{1,tickers,nil}
+	err = templ.Execute(response, data)
+	if err != nil { panic(err) }
 }
 
 func ShowEthXbt(response http.ResponseWriter, request *http.Request) {
+	funcs := template.FuncMap{"divide": func(a, b int) int { return a / b }}
+	contents, err := ioutil.ReadFile("pages/show.html")
+	if err != nil { panic(err) }
+	templ, err := template.New("show").Funcs(funcs).Parse(string(contents))
+	if err != nil { panic(err) }
 	tickers := model.RetrieveTickers(request, "ETHXBT", "-Time")
-	for _, ticker := range tickers { fmt.Fprint(response, ticker.Json) }
+	data := ShowTemplate{1,nil,tickers}
+	err = templ.Execute(response, data)
+	if err != nil { panic(err) }
 }
 
 func Show(response http.ResponseWriter, request *http.Request) {
-	ShowXbtZar(response, request)
-	ShowEthXbt(response, request)
+	funcs := template.FuncMap{"divide": func(a, b int) int { return a / b }}
+	contents, err := ioutil.ReadFile("pages/show.html")
+	if err != nil { panic(err) }
+	templ, err := template.New("show").Funcs(funcs).Parse(string(contents))
+	if err != nil { panic(err) }
+	xbtzar := model.RetrieveTickers(request, "XBTZAR", "-Time")
+	ethxbt := model.RetrieveTickers(request, "ETHXBT", "-Time")
+	data := ShowTemplate{2,xbtzar,ethxbt}
+	err = templ.Execute(response, data)
+	if err != nil { panic(err) }
 }
