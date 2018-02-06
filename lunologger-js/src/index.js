5571cb9248dcfeb6514696610a9e55938ea0cfa5
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-02-06 01:22:07 +0000

    [js] Use ES6

--- lunologger-js/src/index.js
+++ lunologger-js/src/index.js
@@ -1,11 +1,12 @@
-const request = require('request-promise');
-const datastore = require('@google-cloud/datastore')();
+import request from 'request-promise';
+import Datastore from '@google-cloud/datastore';
+const datastore = Datastore()
 
-exports.fetch = ({}, res) => {
+export function fetch({}, response) {
   request.get({uri: 'https://api.mybitx.com/api/1/ticker', qs: {pair: 'XBTZAR'}, json: true}).then(ticker => {
     ticker.timestamp = new Date(ticker.timestamp);
     datastore.save({key: datastore.key(['Ticker']), data: ticker}).then(() => {
-      res.status(200).send(JSON.stringify(ticker));
-    });
+      response.status(200).send(JSON.stringify(ticker));
+    });    
   });
-};
+}
