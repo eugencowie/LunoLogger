6d7056a5af049e4ee018eedbbe7c3aaf3091a898
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-02-16 15:05:59 +0000

    [python] Show or dump daily tickers

--- lunologger-python/main.py
+++ lunologger-python/main.py
@@ -9,17 +9,24 @@ def fetch(pairs='xbtzar+ethxbt'):
     return '\n'.join([model.fetch_ticker(pair.upper()).json for pair in pairs.split('+')])
 
 @app.route('/dump/')
-@app.route('/dump/<pairs>')
-def dump(pairs='xbtzar+ethxbt'):
-    return '\n'.join([''.join([t.json for t in model.retrieve_tickers(pair.upper(), 2000)]) for pair in pairs.split('+')])
+@app.route('/dump/<period>')
+@app.route('/dump/<period>/<pairs>')
+def dump(period='hourly', pairs='xbtzar+ethxbt'):
+    result = []
+    if period == 'hourly': result = [''.join([t.json for t in model.retrieve_tickers(pair.upper(), 2000)]) for pair in pairs.split('+')]
+    if period == 'daily': result = [''.join([t.json for t in model.retrieve_tickers(pair.upper(), 2000) if t.timestamp.hour == 0]) for pair in pairs.split('+')]
+    return '\n'.join(result)
 
 @app.route('/')
 @app.route('/show/')
-@app.route('/show/<raw_pairs>')
-def show(raw_pairs='xbtzar+ethxbt'):
-    pairs = raw_pairs.split('+')
-    tickers = [model.retrieve_tickers_asc(pair.upper(), 50) for pair in pairs]
-    return flask.render_template('show.html', columns=len(pairs), pairs=tickers)
+@app.route('/show/<period>')
+@app.route('/show/<period>/<pairs>')
+def show(period='hourly', pairs='xbtzar+ethxbt'):
+    pairs_list = pairs.split('+')
+    tickers = []
+    if period == 'hourly': tickers = [model.retrieve_tickers_asc(pair.upper(), 50) for pair in pairs_list]
+    if period == 'daily': tickers = [[t for t in model.retrieve_tickers_asc(pair.upper(), 50) if t.timestamp.hour == 0] for pair in pairs_list]
+    return flask.render_template('show.html', columns=len(pairs_list), pairs=tickers)
 
 @app.route('/test/')
 @app.route('/test/<pairs>')
